0d848ffd9a0ae3ee149d30442e0acf026207c96c
Heather <k504222@gmail.com>
2021-05-05 10:18:44 +0800

    add 'good' filter

--- read2.py
+++ read2.py
@@ -15,3 +15,11 @@ for d in data:
 		new.append(d)
 print('一共有',len(new),'筆留言小於100')
 print(new[0])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good),'筆留言包含good這個字')
+print(good[0])

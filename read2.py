00ca87ca7478d660815beff3d36f543d95676e9a
Heather <k504222@gmail.com>
2021-05-05 10:11:48 +0800

    add filter

--- read2.py
+++ read2.py
@@ -8,3 +8,10 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均長度為',sum_len/len(data),'個字')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆留言小於100')
+print(new[0])
